652423f56e2469a856c7e9c0362db020c7f2add4
Charles Hancock <forscyvus@gmail.com>
2014-11-07 00:40:40 -0500

    TEST POST PLEASE IGNORE

---
+++ beamerBot.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import tweepy
+
+with open('keyz.txt','r') as f:
+    consumerKey = f.readline().strip()
+    consumerSecret = f.readline().strip()
+    accessToken = f.readline().strip()
+    accessTokenSecret = f.readline().strip()
+
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+
+api = tweepy.API(auth)
+
+api.update_status("test post please ignore")
