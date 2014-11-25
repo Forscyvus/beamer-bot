5801dba25e5cf5ad663bd32de39d9a9f275b5bda
Charles Hancock <forscyvus@gmail.com>
2014-11-25 02:32:54 -0500

    Update tweets based on those followed

--- beamerBot.py
+++ beamerBot.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import tweepy, sqlite3
+import tweepy, sqlite3, re
 
 
 
@@ -19,8 +19,53 @@ db = sqlite3.connect('db/db')
 
 cursor = db.cursor()
 
+def initializeDB():
+    cursor.execute("CREATE TABLE Tweets(tweetId INTEGER, authorId INTEGER, text TEXT)")
+    db.commit()
+
+def updateTweetsForUser(userId):
+    #get the most recent ones without getting ones we've already got.
+    cursor.execute("""SELECT MAX(tweetId) as MAXID from Tweets
+    WHERE authorId=?""", (userId,))
+    minId = cursor.fetchone()[0]
+    if minId == None:
+        tweets = api.user_timeline(userId, count=200)
+    else:
+        tweets = api.user_timeline(userId, count=200, since_id=minId)
+
+    while len(tweets) > 0:
+        #add tweets to DB
+        for tweet in tweets:
+            #filter out mentions, links, RTs
+            if not re.search(r'https?:\/\/.*[\r\n]*', tweet.text) and not re.search(r'RT @', tweet.text):
+                
+                tweet.text = re.sub('@','@.',tweet.text)
+
+                query = """INSERT INTO Tweets(tweetId, authorId, text)
+                VALUES(?,?,?)
+                """
+                cursor.execute(query, (tweet.id, userId, tweet.text))
+                print tweet.author.screen_name, tweet.text
+                print "--------------"
+
+        #get more tweets
+        maxId = tweets[-1].id
+        #maxid-1 should work because there's no way one user got consecutive tweets. No way.
+        tweets = api.user_timeline(userId, count=200, max_id=maxId-1, since_id=minId)
+    db.commit()
+
+def updateTweets():
+    friends = api.friends_ids(api.me().id)
+    for friendId in friends:
+        updateTweetsForUser(friendId)
+
+#initializeDB()
+updateTweets()
 
 
-
-
-
+    
+    
+    
+    
+    
+    
